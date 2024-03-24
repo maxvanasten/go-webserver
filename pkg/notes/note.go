325066274dcf0a019657a8c19f434e4a6418c3f2
maxvanasten <maxvasten@gmail.com>
2024-03-24 15:01:53 +0100

    Automated commit

--- pkg/notes/note.go
+++ pkg/notes/note.go
@@ -1,35 +1,37 @@
 package notes
 
 import (
-    "net/http"
-    "encoding/json"
+	"encoding/json"
+	"net/http"
+    "html/template"
 )
 
 var notes_db = []Note{}
 
 // Note is a struct that represents a note
 type Note struct {
-    Title string
-    Body string
+	Title string
+	Body  string
 }
 
 func GetNoteHandler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("GET /notes"))
-    for i := range notes_db {
-        w.Write([]byte(notes_db[i].Title))
-    }
+	w.Write([]byte("GET /notes"))
+	
+
+	tmpl := template.Must(template.ParseFiles("public/notes.html"))
+	tmpl.Execute(w, notes_db)
 }
 
 func CreateNoteHandler(w http.ResponseWriter, r *http.Request) {
-    var note Note
-    err := json.NewDecoder(r.Body).Decode(&note)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
+	var note Note
+	err := json.NewDecoder(r.Body).Decode(&note)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-    notes_db = append(notes_db, note)
-    w.Write([]byte("POST /notes"))
-    w.Write([]byte(note.Title))
-    w.Write([]byte(note.Body))
+	notes_db = append(notes_db, note)
+	w.Write([]byte("POST /notes"))
+	w.Write([]byte(note.Title))
+	w.Write([]byte(note.Body))
 }
