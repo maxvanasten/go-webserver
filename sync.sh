e4bab01c246bdb33ae2e4f793887552d8eed5d94
maxvanasten <maxvasten@gmail.com>
2024-03-24 14:25:13 +0100

    ./sync.sh update

--- sync.sh
+++
@@ -1,9 +0,0 @@
-# Update git repo
-git add .
-git commit -m "./sync.sh update"
-git push
-
-# Stop running server and update files
-ssh max@172.233.59.9 'tmux kill-session -t go-webserver; /home/max/scripts/update.sh'
-./scripts/cp_tls.sh
-ssh max@172.233.59.9 '/home/max/scripts/unpack-ssl-bundle.sh; tmux new-session -d -s go-webserver /home/max/scripts/start-server.sh'
